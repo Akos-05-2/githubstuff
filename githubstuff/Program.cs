4178812639e9c85d96e3dc5eb233250871e36ae0
Akos-05-02 <olajkar.akos.botond_2020c@bolyai-szakkozep.hu>
2024-09-11 08:45:54 +0200

    Add project files.

---
+++ githubstuff/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubstuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //azért a lófasznak is van vége
+            List<int> rndm_nums = new List<int>();
+            Random rndm = new Random();
+            for (int i = 0; i <= 100; i++)
+            {
+                rndm_nums.Add(rndm.Next(1, 101));
+            }
+            foreach (var item in rndm_nums)
+            {
+                Console.WriteLine(item + ", ");
+            }
+        }
+    }
+}
